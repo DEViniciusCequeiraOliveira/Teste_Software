0d39b7bb668a405fbdfa108f4af148c0cbe49cff
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2022-10-05 13:51:32 -0300

    atividade quadrante

---
+++ Quadrante.py
@@ -0,0 +1,13 @@
+posX = int(input("digite a posição X: "))
+posY = int(input("digite a posição Y: "))
+
+if posX > 0 and posY > 0:
+    print("primeiro")
+elif posX > 0 and posY < 0:
+    print("quarto")
+elif posX < 0 and posY < 0:
+    print("terceiro")
+elif posX < 0 and posY > 0:
+    print("segundo")
+else:
+    print()
