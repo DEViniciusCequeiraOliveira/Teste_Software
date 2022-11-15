1473a239d6c3e5ca245e8a70ed27a1029185d923
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2022-11-15 10:53:08 -0300

    falta acrescentar alguns metodos

--- Etl/main.py
+++ Etl/main.py
@@ -2,11 +2,15 @@ from interface_usuario import InterfaceUsuario
 from repositories.book_repository import BookRepository
 from repositories.customer_repository import CustomerRepository
 from repositories.order_repository import OrderRepository
+from service.open_book_csv import OpenBookCsv
 
 customer_repository = CustomerRepository()
 order_repository = OrderRepository()
 book_repository = BookRepository()
 interface_usuario = InterfaceUsuario(customer_repository, book_repository, order_repository)
+
+open_book_csv = OpenBookCsv(book_repository)
+open_book_csv.loading_book()
 
 while True:
     menu_option = interface_usuario.principal_menu()
--- Etl/service/open_book_csv.py
+++ Etl/service/open_book_csv.py
@@ -2,15 +2,16 @@ from entities.book import Book
 from repositories.book_repository import BookRepository
 from service.format_price import FormatPrice
 
+
 class OpenBookCsv:
-    
+    def __init__(self, book_repository: BookRepository):
+        self.book_repository = book_repository
+
     file_book = list(open("books.csv", "r", encoding="utf-8"))
 
-    def loading_book(self, book_repository : BookRepository):
+    def loading_book(self):
         for book in self.file_book[1:]:
             list_book = book.split(";")
             book = Book(int(list_book[0]), list_book[1], list_book[2], list_book[3],
-                            list_book[4], FormatPrice().format_str_price_to_float(list_book[5]))
-            book_repository.list_books.append(book)
-
-
+                        list_book[4], FormatPrice().format_str_price_to_float(list_book[5]))
+            self.book_repository.list_books.append(book)
